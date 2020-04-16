eb041dc9a825276612014ed7b755295bcaae9c67
Eric Fritz <eric@eric-fritz.com>
2020-04-16 12:14:48 -0500

    Add emoji tests.

--- cmd/precise-code-intel-api-server/server/bloom_filter_test.go
+++ cmd/precise-code-intel-api-server/server/bloom_filter_test.go
@@ -8,56 +8,60 @@ import (
 	"testing"
 )
 
-var testFiles = []string{
-	"64kb.16",
-	"64kb.08",
-	"64kb.24",
-	"64kb.32",
-	"32kb.16",
-	"32kb.08",
-	"32kb.24",
-	"32kb.32",
-	"96kb.16",
-	"96kb.08",
-	"96kb.24",
-	"96kb.32",
-	"128kb.16",
-	"128kb.08",
-	"128kb.24",
-	"128kb.32",
-}
-
 func TestTestTypeScriptGeneratedBloomFilters(t *testing.T) {
-	loremIpsum := readTestWords(t, "lorem-ipsum")
-	corporateIpsum := readTestWords(t, "corporate-ipsum")
+	testCases := []struct {
+		filterFile  string
+		includeFile string
+		excludeFile string
+	}{
+		{filterFile: "64kb.16", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "64kb.08", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "64kb.24", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "64kb.32", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "32kb.16", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "32kb.08", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "32kb.24", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "32kb.32", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "96kb.16", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "96kb.08", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "96kb.24", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "96kb.32", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "128kb.16", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "128kb.08", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "128kb.24", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "128kb.32", includeFile: "lorem-ipsum", excludeFile: "corporate-ipsum"},
+		{filterFile: "emojis", includeFile: "emojis", excludeFile: "lorem-ipsum"},
+	}
 
-	for _, filename := range testFiles {
-		content, err := ioutil.ReadFile(fmt.Sprintf("test-data/filters/%s", filename))
-		if err != nil {
-			t.Fatalf("unexpected error reading test file %s", filename)
-		}
-
-		raw, err := hex.DecodeString(strings.TrimSpace(string(content)))
-		if err != nil {
-			t.Fatalf("unexpected error decoding test file %s", filename)
-		}
-
-		buckets, m, k, err := decodeFilter(raw)
-		if err != nil {
-			t.Fatalf("unexpected error decoding filter: %s", err)
-		}
-
-		for _, v := range loremIpsum {
-			if !test(v, buckets, m, k) {
-				t.Errorf("expected %s to be in bloom filter", v)
+	for _, testCase := range testCases {
+		t.Run(testCase.filterFile, func(t *testing.T) {
+			content, err := ioutil.ReadFile(fmt.Sprintf("test-data/filters/%s", testCase.filterFile))
+			if err != nil {
+				t.Fatalf("unexpected error reading: %s", err)
 			}
-		}
 
-		for _, v := range corporateIpsum {
-			if test(v, buckets, m, k) {
-				t.Errorf("expected %s not to be in bloom filter", v)
+			raw, err := hex.DecodeString(strings.TrimSpace(string(content)))
+			if err != nil {
+				t.Fatalf("unexpected error decoding: %s", err)
 			}
-		}
+
+			buckets, m, k, err := decodeFilter(raw)
+			if err != nil {
+				t.Fatalf("unexpected error decoding filter: %s", err)
+			}
+
+			for _, v := range readTestWords(t, testCase.includeFile) {
+				if !test(v, buckets, m, k) {
+					t.Errorf("expected %s to be in bloom filter", v)
+				}
+			}
+
+			for _, v := range readTestWords(t, testCase.excludeFile) {
+				if test(v, buckets, m, k) {
+					t.Errorf("expected %s not to be in bloom filter", v)
+				}
+			}
+		})
 	}
 }
 
